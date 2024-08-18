88d826332fbe5ecb15b15fd99288713123f057fd
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-18 14:54:42 +0000

    added the random function

--- app/cta/page.tsx
+++ app/cta/page.tsx
@@ -3,9 +3,17 @@ import SmNavbar from "@/components/hand-crafted/sm-navbar";
 import React from "react";
 
 const Buttons = () => {
+
+
+  const randomNumberGenerator = (min: number, max: number) => {
+
+    return Math.random() * (max - min) + min;
+  }
+
+
   return (
     <div className="min-h-screen flex items-center justify-center flex-col">
-      <Button text="2(Bkm)" starLength={10} />
+      <Button text="2(Bkm)" starLength={randomNumberGenerator(2,5)} />
       <SmNavbar />
     </div>
   );
