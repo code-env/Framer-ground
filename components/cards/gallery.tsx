dc01010c5b7d1935a0a36c2160d5507c8e485701
code-env <org.codeen@gmail.com>
2024-08-26 12:15:06 +0100

    feat: added the second gallery

--- components/cards/gallery.tsx
+++ components/cards/gallery.tsx
@@ -1,60 +1,149 @@
 "use client";
 
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+interface Element {
+  id: number;
+  width: number;
+  img: string;
+}
 
-const Gallery = () => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+interface Column {
+  id: number;
+  elements: Element[];
+}
 
-  const images = [
-    { src: "/her/image1.jpeg", alt: "Image 1" },
-    { src: "/her/image2.jpeg", alt: "Image 2" },
-    { src: "/her/image3.jpeg", alt: "Image 3" },
-    { src: "/her/image4.jpeg", alt: "Image 4" },
-  ];
+const items: Column[] = [
+  {
+    id: 1,
+    elements: [
+      { id: 1, width: 250, img: "/others/photo-1.jpg" },
+      { id: 2, width: 100, img: "/others/photo-2.jpg" },
+    ],
+  },
+  {
+    id: 2,
+    elements: [
+      { id: 3, width: 100, img: "/others/photo-6.jpg" },
+      { id: 4, width: 250, img: "/others/photo-7.jpg" },
+    ],
+  },
+];
+
+const GalleryOne = () => {
+  const [activeItem, setActiveItem] = useState<Element | null>(null);
+
+  const allElements = items.flatMap((column) => column.elements);
+
+  const handleItemClick = (ele: Element) => {
+    setActiveItem(ele);
+  };
 
   return (
-    <div className="h-screen center w-full">
+    <div className="h-screen center w-full flex flex-col gap-5 relative">
       <motion.div
-        className={cn("grid grid-cols-2 gap-4 relative", {
-          "flex items-center flex-col": activeIndex !== null,
-        })}
+        className={cn("flex flex-col gap-5")}
         layout
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
-        {images.map((item, index) => (
+        {items.map((column) => (
           <motion.div
-            key={index}
-            className={cn(
-              "rounded-3xl after:bg-black/50 after:inset-0 after:h-full after:w-full after:absolute relative center text-white font-bold cursor-pointer overflow-hidden",
-              {
-                "order-last ml-auto text-5xl absolute right-[120%] -bottom-16 after:bg-transparent":
-                  activeIndex === index,
-                "mr-[-200px] rounded-xl":
-                  activeIndex !== null && activeIndex !== index,
-              }
-            )}
-            layout
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-            onClick={() => setActiveIndex(activeIndex === index ? null : index)}
+            className={cn("flex items-center justify-center gap-5")}
+            key={column.id}
             animate={{
-              height:
-                activeIndex === index ? 300 : activeIndex !== null ? 50 : 80,
-              width:
-                activeIndex === index ? 300 : activeIndex !== null ? 50 : 80,
+              opacity: activeItem !== null ? 0 : 1,
+              willChange: "auto",
             }}
           >
-            <img
-              src={item.src}
-              alt={item.alt}
-              className="w-full object-cover"
-            />
+            {column.elements.map((ele, index) => (
+              <Gallery
+                item={ele}
+                key={index}
+                onClick={() => setActiveItem(ele)}
+              />
+            ))}
           </motion.div>
         ))}
       </motion.div>
+
+      {activeItem && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, willChange: "auto" }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="absolute inset-0 w-full h-screen overflow-hidden"
+        >
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              key={activeItem.id}
+              className="w-full h-screen flex items-center justify-center gap-10 overflow-hidden "
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              layout
+            >
+              <motion.div
+                layoutId={`card-${activeItem.id}`}
+                className="w-[400px] h-[400px] rounded-3xl center font-bold text-5xl cursor-pointer overflow-hidden z-10"
+                onClick={() => setActiveItem(null)}
+              >
+                <img
+                  src={activeItem.img}
+                  alt=""
+                  className="w-full object-cover h-full"
+                />
+              </motion.div>
+              <motion.div
+                className="flex flex-col gap-4 justify-center items-center"
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.3 }}
+              >
+                {allElements
+                  .filter((ele) => ele.id !== activeItem.id)
+                  .map((ele) => (
+                    <Gallery
+                      key={ele.id}
+                      item={ele}
+                      onClick={() => handleItemClick(ele)}
+                      isSmall
+                    />
+                  ))}
+              </motion.div>
+            </motion.div>
+          </AnimatePresence>
+        </motion.div>
+      )}
     </div>
   );
 };
 
-export default Gallery;
+const Gallery = (props: {
+  item: Element;
+  onClick: () => void;
+  isSmall?: boolean;
+}) => {
+  return (
+    <motion.div
+      style={{
+        width: props.isSmall ? 80 : props.item.width,
+        height: props.isSmall ? 80 : 150,
+      }}
+      className={cn(
+        "rounded-2xl cursor-pointer text-3xl center overflow-hidden relative"
+      )}
+      layoutId={`card-${props.item.id}`}
+      onClick={props.onClick}
+    >
+      <motion.img
+        src={props.item.img}
+        alt=""
+        className="w-full object-cover h-full"
+        whileHover={{ scale: 1.05 }}
+        transition={{
+          duration: 0.3,
+        }}
+      />
+    </motion.div>
+  );
+};
+
+export default GalleryOne;
